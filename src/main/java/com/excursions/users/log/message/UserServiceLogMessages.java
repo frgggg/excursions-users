1045133b22332a194033b25430836d9dd56ab75c
Fedor <zhar.feda@yandex.ru>
2020-02-20 14:29:19 +0300

    fix logs for coins up/down

--- src/main/java/com/excursions/users/log/message/UserServiceLogMessages.java
+++ src/main/java/com/excursions/users/log/message/UserServiceLogMessages.java
@@ -8,8 +8,8 @@ public class UserServiceLogMessages {
     public static final String USER_SERVICE_LOG_GET_ALL_USERS = "Get all users";
 
     public static final String USER_SERVICE_LOG_UP_COINS_BY_USER = "User with id {}: up coins for {} by user";
-    public static final String USER_SERVICE_LOG_DOWN_COINS_BY_USER = "User with id {}: up coins for {} by user";
+    public static final String USER_SERVICE_LOG_DOWN_COINS_BY_USER = "User with id {}: down coins for {} by user";
 
     public static final String USER_SERVICE_LOG_UP_COINS_BY_EXCURSION = "User with id {} up coins for {} by excursion";
-    public static final String USER_SERVICE_LOG_DOWN_COINS_BY_EXCURSION = "User with id {} up coins for {} by excursion";
+    public static final String USER_SERVICE_LOG_DOWN_COINS_BY_EXCURSION = "User with id {} down coins for {} by excursion";
 }
