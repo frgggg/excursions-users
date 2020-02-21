7e962283439f8146c27c962d2618e79b529062e7
Fedor <zhar.feda@yandex.ru>
2020-02-21 16:17:34 +0300

    commit for help

---
+++ src/main/resources/db/migration/V0__initial.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE users;
+
+CREATE TABLE users (
+id int8 not null,
+coins int8 not null check (coins>=0),
+coins_last_update timestamp not null,
+name varchar(90) not null, primary key (id)
+);
+
+create sequence hibernate_sequence start 1 increment 1;
--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,5 +0,0 @@
-create sequence hibernate_sequence start 1 increment 1;
-
-create table users (
-id int8 not null, coins int8 not null check (coins>=0), coins_last_update timestamp not null, name varchar(90) not null, primary key (id)
-);
